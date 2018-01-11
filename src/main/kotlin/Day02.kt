9dad364d3908b8abf297b625fb1df7811a638b1a
Andre Perkins <andre.perkins@viacom.com>
2018-01-11 15:11:56 -0500

    finished day 2 challenges

--- src/main/kotlin/Day02.kt
+++ src/main/kotlin/Day02.kt
@@ -1,18 +1,37 @@
 object Day02 {
 
-  fun part1(input: String): Int {
-    var checkSum = 0
-    val lines = input.split("\n")
-    lines.forEach {
-      println("numbers:$it")
-      val numbers = it.split("\t").map { Integer.parseInt(it) }
-      checkSum += (numbers.max()!! - numbers.min()!!)
+    fun part1(input: String): Int {
+        var checkSum = 0
+        val lines = input.split("\n")
+        lines.forEach {
+            val numbers = it.split("\t").map { Integer.parseInt(it) }
+            checkSum += (numbers.max()!! - numbers.min()!!)
+        }
+        return checkSum
     }
-    return checkSum
-  }
 
-  fun part2(input: String): Int {
-    TODO()
-  }
+    fun part2(input: String): Int {
+        var checkSum = 0
+        val lines = input.split("\n")
+        lines.forEach {
+            println("numbers:$it")
+            val (x, y) = findDivisors(it.split("\t").map { Integer.parseInt(it) })
+            checkSum += (x / y)
+        }
+        return checkSum
+    }
+
+    private fun findDivisors(numbers: List<Int>): Pair<Int, Int> {
+        numbers.forEachIndexed { i, x ->
+            numbers.forEachIndexed { j, y ->
+                if (i != j) {
+                    if (x % y == 0) {
+                        return Pair(x, y)
+                    }
+                }
+            }
+        }
+        throw IllegalArgumentException("List does not contain ints that divide into each other. Numbers:$numbers")
+    }
 
 }
